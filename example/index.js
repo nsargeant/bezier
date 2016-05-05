60475036e63ef859684a02509000aba960eaab97
nsargeant <nathan.s.sargeant@gmail.com>
2016-05-05 02:50:34 -0600

    Added ability to upload formatted json file

--- example/index.js
+++ example/index.js
@@ -17,18 +17,26 @@ document.addEventListener('draw', function () {
 
 var button = document.getElementById('points');
 button.addEventListener('click', function (evnt) {
-	canvasItems.forEach(function (item, i) {
-		console.log(item.toString());
-	});
+	var points = JSON.stringify(canvasItems.map(function (item) {
+		return {
+			start: item.start,
+			p1: item.p1,
+			p2: item.p2,
+			end: item.end
+		};
+	}), null, 2);
+	document.querySelector('.display-points').classList.remove('hidden');
+	var pointsText = document.getElementById('points-text');
+	pointsText.innerHTML = points;
 });
 
 var addBtn = document.getElementById('add');
 addBtn.addEventListener('click', function (evnt) {
 	var coords = {
-		start: [10,10],
-		p1: [30,10],
-		p2: [30,30],
-		end: [10,30]
+		start: [10, 10],
+		p1: [30, 10],
+		p2: [30, 30],
+		end: [10, 30]
 	};
 	canvasItems.push(new Bezier(ctx, coords));
 	document.dispatchEvent(new Event('draw'));
@@ -38,5 +46,23 @@ addBtn.addEventListener('click', function (evnt) {
 var save = document.getElementById('save');
 save.addEventListener('click', function (evnt) {
 	var url = canvas.toDataURL();
-	window.open(url,'_blank');
+	window.open(url, '_blank');
 });
+
+var open = document.getElementById('open');
+open.onchange = function (evt) {
+	var tgt = evt.target || window.event.srcElement,
+		files = tgt.files;
+
+	// FileReader support
+	if (FileReader && files && files.length) {
+		var fr = new FileReader();
+		fr.onload = function () {
+			canvasItems = JSON.parse(fr.result).map(x => {
+				return new Bezier(ctx, x);
+			});
+			document.dispatchEvent(new Event('draw'));
+		}
+		fr.readAsText(files[0]);
+	}
+}
